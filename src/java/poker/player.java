a7e088f502a74169d6d08955ccf9c9f4384e9904
Meaza feleke <72835732+Meaza-art@users.noreply.github.com>
2022-03-29 12:44:59 -0500

    Update player.java

--- src/java/poker/player.java
+++ src/java/poker/player.java
@@ -13,26 +13,46 @@ public class Player
 	private int id;
 
 }
-public Player(int id) {
+     public Player(int id) {
 		this.setName("Player " + id);
-		
+		this.setCurrentBet();
+       		this.setBalance();
 		this.setHand(new ArrayList<Hand>());
-	}
+	                     }
+    public double getCurrentBet()
+    {
+        return currentBet;
+    }
+    public void setCurrentBet(double currentBet) 
+    {
+	this.currentBet = currentBet;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setBalance(double balance)
+    {
+	this.balance = balance;
+    }   
 
-	public String getName() {
-		return name;
-	}
+    public String getName()
+    {
+	return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+     public void setName(String name)
+     {
+        this.name = name;
+     }
 
     public Hand getHand()
     {
         return hand;
     }
-    public void setHand(Hand hand) {
-		this.hand = hand;
-	}
+    public void setHand(Hand hand)
+    {
+	this.hand = hand;
+}
 }
 
