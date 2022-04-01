e6b53a357414c9bf2a1c876f20574fe31b7ec08d
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-04-01 16:09:38 -0500

    Updated UserEvent

--- src/pokerServer/UserEvent.java
+++ src/pokerServer/UserEvent.java
@@ -4,6 +4,7 @@ public class UserEvent {
     public UserEvenType event;
     public int playerID;
     public String roomID;
+    public String msg;
     
     public enum UserEvenType {
         RAISE, CALL, CHECK, FOLD, CREATE_ROOM, JOIN_ROOM, START_MATCH
