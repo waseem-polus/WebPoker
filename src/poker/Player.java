4b647ca186b2f27da3a1d7efd2caf394ec35f340
MTrinh524 <minhtrinh524@gmail.com>
2022-04-01 17:15:08 -0400

    Updated Player

--- src/poker/Player.java
+++ src/poker/Player.java
@@ -1,5 +1,7 @@
 package poker;
 
+import cards.CardDeck;
+
 public class Player {
     private String name;
     private double balance;
@@ -15,14 +17,46 @@ public class Player {
         this.id = id;
     }
 
-    public void placeBet(double amount) {
+    public void exchangeCard(int[] cardIndex, CardDeck deck) {
+        hand.exchangeCard(cardIndex, deck);
+    }
+
+    public void dealHand(CardDeck deck) {
+        hand.dealHand(deck);
+    }
+
+    public double placeBet(double amount) {
+        double bet = amount;
         if (amount <= balance) {
             this.currentBet += amount;
             this.balance -= amount;
         }
+        else {
+            this.currentBet += this.balance;
+            bet = this.balance;
+            this.balance = 0;
+        }
+
+        return bet;
     }
+
+    public double getcurrentBet() {
+        return currentBet;
+    }
+
+    public int getID() {
+        return id;
+    } 
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    // public int hashCode() {
+    //     return;
+    // }
+
+    public boolean equals(Player p) {
+        return this.id == p.id;
     }
 }
