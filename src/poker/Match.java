f123fbe474d283f4cc2d83811ad6d9f52fd71230
vfa7725 <victor.arowosafe@mavs.uta.edu>
2022-04-01 16:19:39 -0500

    Implemented call, Raise and exchange card methods

--- src/poker/Match.java
+++ src/poker/Match.java
@@ -1,15 +1,19 @@
 package poker;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cards.CardDeck;
+import pokerServer.UserEvent;
 
 
 public class Match {
     private CardDeck deck;
     private double bettingPool;
     private MatchRound round;
-    private HashMap<Integer, Player> players;
+    private ArrayList<Player> activePlayers;
+    private int turnNumber;
+    private int currentPlayerID;
 
     public enum MatchRound {
         FIRST_BETTING, DRAWING, SECOND_BETTING, SHOWDOWN;
@@ -19,7 +23,23 @@ public class Match {
 
     }
 
-    public void startMatch() {
+    public Match(double playerBalance) {
+
+    }
+
+    public void addPlayer(Player player){
+
+    }
+
+    public void removerPlayer(int playerID){
+        
+    }
+
+    public void onEvent(UserEvent event){
+
+    }
+
+    public void onStartMatch() {
 
     }
 
@@ -35,19 +55,48 @@ public class Match {
 
     }
 
-    public void fold(int playerID) {
+    public void onFold(int playerID) {
 
     }
 
-    public void check(int playerID) {
-
+    public void onCheck(int playerID) {
+        
     }
 
-    public void call(int playerID) {
-
+    public void onCall(int playerID) {
+        if(round == MatchRound.FIRST_BETTING || round == MatchRound.SECOND_BETTING){
+            for(int i=0;i<activePlayers.size();i++){
+                int IDCheck=activePlayers.get(i).getID();
+                if(IDCheck == playerID){
+                    if(activePlayers.get(i-1).getcurrentBet() > activePlayers.get(i).getcurrentBet()){
+                        double addToPool=activePlayers.get(i).placeBet((activePlayers.get(i-1).getcurrentBet()-activePlayers.get(i).getcurrentBet()));
+                        bettingPool +=addToPool;
+                    }
+                }
+            }
+        }
     }
 
-    public void raise(int playerID, double amount) {
+    public void onRaise(int playerID, double amount) {
+        if(round == MatchRound.FIRST_BETTING || round == MatchRound.SECOND_BETTING){
+            for(int i=0;i<activePlayers.size();i++){
+                int IDCheck=activePlayers.get(i).getID();
+                if(IDCheck == playerID){
+                    double addToPool=activePlayers.get(i).placeBet(amount);
+                    bettingPool +=addToPool;
+                }
+            }
+        }
+    }
 
+    public void onExchangeCards(int playerID, int[] cardIndex){
+        if(round == MatchRound.DRAWING){
+            for(int i=0;i<activePlayers.size();i++){
+                int IDCheck=activePlayers.get(i).getID();
+                if(IDCheck == playerID){
+                    activePlayers.get(i).exchangeCard(cardIndex,deck);
+                }
+            }
+        }
     }
 }
