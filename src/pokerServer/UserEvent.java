ec15d67cf578d3b72fecd5eb84381878385dab45
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-04-01 17:09:04 -0500

    Updated UserEvent

--- src/pokerServer/UserEvent.java
+++ src/pokerServer/UserEvent.java
@@ -1,16 +1,25 @@
 package pokerServer;
 
+
+
 public class UserEvent {
-    public UserEvenType event;
+    public UserEventType event;
     public int playerID;
     public String roomID;
     public String msg;
     
-    public enum UserEvenType {
-        RAISE, CALL, CHECK, FOLD, CREATE_ROOM, JOIN_ROOM, START_MATCH
+    public enum UserEventType {
+        RAISE, CALL, CHECK, FOLD, CREATE_ROOM, JOIN_ROOM, START_MATCH, EXCHANGE_CARDS
     }
 
     public UserEvent () {
-
     }
+
+    // public int[] msgToIntArray() {
+    //     return int[];
+    // }
+
+    // public double msgToDouble() {
+
+    // }
 }
