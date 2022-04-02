7c6115b3defbd822fc0a76cdec51e00ad43a5860
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2022-04-02 15:07:16 -0500

    Added comments to exaplin msg and msgArr

--- src/pokerServer/UserEvent.java
+++ src/pokerServer/UserEvent.java
@@ -4,8 +4,8 @@ public class UserEvent {
     public UserEventType event;
     public int playerID;
     public String roomID;
-    public Object msg;
-    public Object[] msgArr;
+    public Object msg; //Can be cast down based on event
+    public Object[] msgArr; //Can be cast down basec on event
     
     public enum UserEventType {
         RAISE, CALL, CHECK, FOLD, CREATE_ROOM, JOIN_ROOM, START_MATCH, EXCHANGE_CARDS, RESTART_MATCH
