a1994e6ac3e98984e05db585565e63fb73852ed0
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-04-17 09:52:08 -0500

    only players with a balance greater than 20 will be allowed into the new match

--- src/main/java/uta/cse3310/pokerServer/Room.java
+++ src/main/java/uta/cse3310/pokerServer/Room.java
@@ -130,7 +130,9 @@ public class Room {
         this.match = new Match();
         for (Player p : players.values()) {
             p.clearBet();
-            this.match.addPlayer(p);
+            if (p.getBalance() > 0) {
+                this.match.addPlayer(p);
+            }
         }
         this.match.onStartMatch();
     }
