2158e0f716cb3fa2a821d85304a0c721f2a0c25d
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-04-27 20:04:57 -0500

    Added test case for CardDeck

--- src/test/java/uta/cse3310/poker/TestCardDeck.java
+++ src/test/java/uta/cse3310/poker/TestCardDeck.java
@@ -5,26 +5,51 @@ import uta.cse3310.cards.CardDeck;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-public class TestCardDeck
-{
+public class TestCardDeck {
 
+    /*
+	 * Author: Minh Trinh
+	 * Last Updated: 4/27/2022
+	 * 
+	 * tests of 52 cards are created when a deck is initialized
+	 */
     @Test
-    public void test_52_cards() throws Exception
-    {
+    public void test_52_cards() throws Exception {
         CardDeck deck = new CardDeck();
         int inc = 0;
 
-        try
-        {
-            while(inc != 52)
-            {
+        try {
+            while (true) {
                 deck.drawCard();
                 inc++;
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
         }
         assertTrue(inc == 52);
+    }
+
+    /*
+	 * Author: Waseem Alkasbutrus
+	 * Last Updated: 4/27/2022
+	 * 
+	 * Tests if shuffle deck shuffles correctly (the cards are not in the same order after being shuffled)
+	 */
+    @Test
+    public void testShuffle() throws Exception {
+        CardDeck deck = new CardDeck();
+        deck.shuffleDeck();
+
+        CardDeck CopyDeck = deck;
+
+        boolean differentOrder = false;
+
+        for(int i = 0; i < 52; i++) {
+            if (!deck.drawCard().equals(CopyDeck.drawCard())) {
+                differentOrder = true;
+                break;
+            }
+        }
+
+        assertTrue(differentOrder);
     }
 }
